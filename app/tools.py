d99527fe8fd90a3c439b0198bc2655e59cd627d5
Macaya25 <afmacaya@miuandes.cl>
2024-03-25 01:04:31 -0300

    Enajenantes añadidos automaticamente por JSON.

--- app/tools.py
+++ app/tools.py
@@ -1,36 +1,68 @@
-from models import Formulario, Enajenante
+from models import Formulario, Enajenante, Persona
 from flask_sqlalchemy import SQLAlchemy
+from dateutil.parser import parse
+from datetime import datetime
 
 def analyzeJSON(db: SQLAlchemy, JSON):
     if JSON['F2890']:
         for single_form in JSON['F2890']:
             newForm = Formulario()
+            enajenantes = []
+
             
-            keys = ['cne', 'bienRaiz', 'fojas', 'fecha_inscripcion', 'num_inscripcion']
+            main_keys = ['cne', 'bienRaiz', 'fojas', 'fecha_inscripcion', 'num_inscripcion']
+            location_keys = ['comuna', 'manzana', 'predio']
+
             for key, value in single_form.items():
-                if key != 'bienRaiz' and key in keys:
+                if key != 'bienRaiz' and key in main_keys:
                     setattr(newForm, key, value)
-                    #print(key, value)
                 elif key == "CNE":
                     setattr(newForm, 'cne', value)
                 elif key == "fechaInscripcion":
-                    setattr(newForm, 'fecha_inscripcion', value)
+                    try:
+                        parse(value)
+                        setattr(newForm, 'fecha_inscripcion', value)
+                    except ValueError:
+                        setattr(newForm, 'fecha_inscripcion', datetime(1900, 1, 1).date())
+                    
+                    
                 elif key == 'nroInscripcion':
                     setattr(newForm, 'num_inscripcion', value)
+
+                elif key == 'bienRaiz':
+                    for k, v in single_form['bienRaiz'].items():
+                        if k == 'comuna':
+                            setattr(newForm, 'comuna', v)
+                        elif k == 'manzana':
+                            setattr(newForm, 'manzana', v)
+                        elif k == 'predio':
+                            setattr(newForm, 'predio', v)
 
                 elif key == 'enajenantes':
                     for singleEnajenante in single_form['enajenantes']:
                         newEnajenante = Enajenante()
-                        if 'RUNRUT' in singleEnajenante.items():
+                        if 'RUNRUT' in singleEnajenante.keys():
                             newEnajenante.run_rut = singleEnajenante['RUNRUT']
-                        if 'porcDerecho' in singleEnajenante.items():
+
+                            person = Persona.query.filter_by(run_rut=newEnajenante.run_rut).first()
+                            if person is None:
+                                person = Persona(run_rut=newEnajenante.run_rut)
+                                db.session.add(person)
+                                db.session.commit()
+
+                        if 'porcDerecho' in singleEnajenante.keys():
                             newEnajenante.porc_derecho = singleEnajenante['porcDerecho']
                         
-                        print(newEnajenante)
+                        enajenantes.append(newEnajenante)
                 
 
+            db.session.add(newForm)
+            db.session.commit()
 
-            print(newForm)
+            for singleEnajenante in enajenantes:
+                singleEnajenante.form_id = newForm.n_atencion
+                db.session.add(singleEnajenante)
+            db.session.commit()
 
     else:
         print('Formato no valido.')
