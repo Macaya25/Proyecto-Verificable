17e947bfbfa202506d6273fbf261ed8f4a2ffa2d
jose Tomas anabalon <43974814+AnabalonJT@users.noreply.github.com>
2024-04-03 22:36:33 -0300

    multipropietario subida ok, falta search

--- app/app.py
+++ app/app.py
@@ -4,9 +4,10 @@ import json
 from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_wtf.csrf import CSRFProtect
 from forms import FormularioForm, JSONForm
-from models import db, Formulario, Persona, Enajenante, Adquirente, Multipropietario
+from models import db, Formulario, Persona, Enajenante, Adquirente, Multipropietario, CNE
 from tools import analyze_json
 from dotenv import load_dotenv
+
 
 
 load_dotenv()
@@ -69,13 +70,47 @@ def form_route():
                 formulario=new_formulario
             )
             db.session.add(new_adquirente)
-
+        new_multipropietarios(form)
         db.session.commit()
         flash('Formulario registrado con éxito!')
         return redirect(url_for('index_route'))
 
     return render_template('form.html', form=form)
 
+def new_multipropietarios(form):
+    for adquiriente in form.adquirentes.data:
+        new_multipropietario(form, adquiriente['run_rut'], adquiriente['porc_derecho'])
+    if form.cne.data == '1':
+        for enajenante in form.enajenantes.data:
+            new_multipropietario(form, enajenante['run_rut'], enajenante['porc_derecho'])
+
+def new_multipropietario(form, rut, derecho):
+    año_vigencia_inicial = form.fecha_inscripcion.data.year
+    año_vigencia_final = None
+    # Lógica para determinar el año de vigencia final
+    multipropietario_anterior = Multipropietario.query.filter_by(
+        comuna=form.comuna.data,
+        manzana=form.manzana.data,
+        predio=form.predio.data
+    ).filter(Multipropietario.ano_vigencia_inicial >= año_vigencia_inicial).first()
+
+    if multipropietario_anterior:
+        año_vigencia_final = multipropietario_anterior.año_vigencia_inicial
+
+    new_multipropietario = Multipropietario(
+        comuna=form.comuna.data,
+        manzana=form.manzana.data,
+        predio=form.predio.data,
+        run_rut=rut,
+        porc_derechos=derecho,
+        fojas=form.fojas.data,
+        ano_inscripcion=form.fecha_inscripcion.data.year,
+        num_inscripcion=form.num_inscripcion.data,
+        fecha_inscripcion=form.fecha_inscripcion.data,
+        ano_vigencia_inicial=año_vigencia_inicial,
+        ano_vigencia_final=año_vigencia_final
+    )
+    db.session.add(new_multipropietario)
 
 @app.route('/forms')
 def forms_route():
@@ -104,22 +139,34 @@ def form_json_route():
 @app.route('/forms/<int:n_atencion>')
 def form_details_route(n_atencion):
     formulario = Formulario.query.get_or_404(n_atencion)
-    return render_template('form_details.html', formulario=formulario)
+    descripcion_cne =obtener_descripcion_cne(formulario.cne)
+    return render_template('form_details.html', formulario=formulario, descripcion_cne = descripcion_cne)
+
+def obtener_descripcion_cne(cne_id):
+    print(cne_id)
+    cne = CNE.query.filter_by(id=cne_id).first()
+    return cne.descripcion if cne else "Descripción no encontrada"
 
 
 @app.route('/multipropietario', methods=['GET', 'POST'])
 def multipropietario_route():
-    if request.method == 'POST':
-        comuna = request.form.get('comuna')
-        manzana = request.form.get('manzana')
-        predio = request.form.get('predio')
-        año = request.form.get('año')
+    form = FormularioForm()
+    search_results = []
 
-        results = Multipropietario.query.filter_by(
-            comuna=comuna, manzana=manzana, predio=predio, año=año).all()
-        return render_template('multipropietario.html', search_results=results)
+    if request.method == 'POST' and form.validate_on_submit():
+        comuna = form.comuna.data
+        manzana = form.manzana.data
+        predio = form.predio.data
+        año = form.fecha_inscripcion
 
-    return render_template('multipropietario.html', search_results=None)
+        search_results = Multipropietario.query.filter_by(
+            comuna=comuna, manzana=manzana, predio=predio).filter(Multipropietario.ano_inscripcion >= año).all()
+        print("hola")
+        return render_template('multipropietario.html', form=form, search_results=search_results)
+    else:
+        search_results = Multipropietario.query.all()
+        return render_template('multipropietario.html',form = form, search_results=search_results)
+
 
 
 if __name__ == '__main__':
